3dd8ca9a296265fe70486b91eb43a3df3cd2e702
BT_C09_06 <mobileappdev@bths.edu>
2026-05-04 13:49:48 -0400

    Initial Commit

---
+++ iOSProject2Trivia/iOSProject2TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSProject2TriviaApp.swift
+//  iOSProject2Trivia
+//
+//  Created by Student on 5/4/26.
+//
+
+import SwiftUI
+
+@main
+struct iOSProject2TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
