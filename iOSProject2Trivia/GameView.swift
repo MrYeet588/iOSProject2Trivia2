c575a6b8020c23a1e4a13392067a1f343e4079f7
BT_C09_20 <mobileappdev@bths.edu>
2026-05-06 14:00:15 -0400

    fix attempts

--- iOSProject2Trivia/GameView.swift
+++ iOSProject2Trivia/GameView.swift
@@ -6,6 +6,21 @@
 //
 
 import SwiftUI
+import Foundation
+
+/*extension String {                ignore this for now
+    var htmlDecoded: String {
+        guard let data = self.data(using: .utf8) else { return self }
+        let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
+            .documentType: NSAttributedString.DocumentType.html,
+            .characterEncoding: String.Encoding.utf8.rawValue
+        ]
+        if let attributedString = try? NSAttributedString(data: data, options: options, documentAttributes: nil) {
+            return attributedString.string
+        }
+        return self
+    }
+}*/
 
 struct GameView: View {
     @State private var questions: [Question] = []
@@ -13,7 +28,27 @@ struct GameView: View {
     var body: some View {
         List {
             ForEach(questions) { q in
-                Text(q.question)
+                //&amp = & | &quot; = " | &#039; = ' | &ndash = - | @eacute; = é
+                if (q.question.contains("&quot;") == true) {
+                    let qwe = q.question.replacingOccurrences(of: "&quot;", with: "\"")
+                    Text(qwe)
+                } else if (q.question.contains("&amp")){
+                    let qwe = q.question.replacingOccurrences(of: "&amp", with: "&")
+                    Text(qwe)
+                } else if (q.question.contains("&#039")){
+                    let qwe = q.question.replacingOccurrences(of: "&#039", with: "\'")
+                    Text(qwe)
+                } else if (q.question.contains("&ndash")){
+                    let qwe = q.question.replacingOccurrences(of: "&ndash", with: "-")
+                    Text(qwe)
+                } else if (q.question.contains("@eacute;")){
+                    let qwe = q.question.replacingOccurrences(of: "&eacute;", with: "é")
+                    Text(qwe)
+                }
+                else {
+                    Text(q.question)
+                }
+                //Text(q.question.htmlDecoded)
             }
         }
         .task {
