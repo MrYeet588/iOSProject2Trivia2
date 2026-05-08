966b67d55474b72a8b08394c3eaae298409bed69
BT_C09_19 <mobileappdev@bths.edu>
2026-05-08 13:38:29 -0400

    IT WORKS HELL YEAH

--- iOSProject2Trivia/MainMenu.swift
+++ iOSProject2Trivia/MainMenu.swift
@@ -11,21 +11,34 @@ struct MainMenu: View{
     var body: some View {
         ZStack {
             
-            Color(background_color_toggle ? .green : .blue)
+            Color(background_color_toggle ? .red : .blue)
                 .ignoresSafeArea(.container, edges: .all)
                 .rotation3DEffect(.degrees(background_color_toggle ? 360 : 0), axis: (1, 1, 1))
-                .animation(.easeInOut(duration: 1).repeatForever(autoreverses: false), value: background_color_toggle)
+                .animation(.easeInOut(duration: 0.04).repeatForever(autoreverses: false), value: background_color_toggle)
                 .onTapGesture {
                     background_color_toggle.toggle()
                 }
             Text("Hello")
-            
+            NavigationStack {
+                VStack{
+                    NavigationLink(destination: {
+                        des1()
+                        
+                    }, label : {
+                        Text("click")
+                    })
+                }
+            }
         }
 
 
     }
 }
-
+struct des1 : View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
 
 #Preview {
     MainMenu()
